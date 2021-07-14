b2de20b5f621dc97e313ad36c16a24335d83507c
samy <samydoreau@gmail.com>
2021-07-14 20:14:43 +0100

    updates

--- tests.py
+++ tests.py
@@ -1,5 +1,18 @@
-one_turf = {'surface': 1.6 * 1.64, 'cost': 20}
-garden = 30
+import os
+import datetime as d
+import shutil
+from pathlib import Path
 
-total_cost = (garden / one_turf['surface']) * one_turf['cost']
-print(f'Total cost is : {total_cost}')
+dir_path = str(Path.home() / "Downloads")
+target_path = f'{Path.home()}/Dropbox/Code/Automation/output/PDF_Economist'
+files_moved = 0
+
+
+today = d.datetime.now().date()
+for file in os.listdir(dir_path):
+    file_time = d.datetime.fromtimestamp(
+        os.path.getctime(dir_path + '/' + file))
+    filename, file_extension = os.path.splitext(f'{dir_path}/{file}')
+    if file_time.date() == today and file_extension == '.pdf':
+        shutil.move(f'{dir_path}/{file}', f'{target_path}/{file}')
+        files_moved += 1
